8e55785d9d3325e11b545aa58a1b708f8d58b7db
Manikandan.M <manikandan.m@dsrc.co.in>
2020-03-26 16:43:49 +0530

    demo project

---
+++ Layout_UI/Pages/viewquestions.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Layout_UI.Pages
+{
+    public class viewquestionsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
